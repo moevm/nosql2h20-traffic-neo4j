c8158938d0dec7b50a462df9911482587afc06f9
Stanislau Dorakh <stanislau_dorakh@epam.com>
2020-11-07 21:33:33 +0300

    Add simple flask app and dockerfile

---
+++ web_app/resources/analytics.py
@@ -0,0 +1,14 @@
+from flask import jsonify
+from flask_restful import Resource
+
+
+class AnalyticsApi(Resource):
+    def get(self):
+        """
+        Method sends to user analytics about database stuff
+
+        Returns:
+            response(flask.Response): json with analytics
+        """
+        # bla bla bla
+        return jsonify({"info": "bla bla bla"})
